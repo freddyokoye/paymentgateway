e167b06d5ec08da45d466128646dc6ec6c1bee5f
Freddy Okoye <44079004+freddyokoye@users.noreply.github.com>
2025-09-02 13:45:51 +0100

    Create VodiaPaymentGateway.php

---
+++ src/Facades/VodiaPaymentGateway.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Vodia\Paymentgateway\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use Vodia\Paymentgateway\Base\Gateways\InstamojoPay;
+use Vodia\Paymentgateway\Base\PaymentGatewayHelpers;
+use Vodia\Paymentgateway\Base\Gateways\SquarePay;
+/**
+ * @see GlobalCurrency
+ * @method static script_currency_list()
+ *
+ * @see PaymentGatewayHelpers
+ * @method static stripe()
+ * @method static paypal()
+ * @method static midtrans()
+ * @method static paytm()
+ * @method static razorpay()
+ * @method static mollie()
+ * @method static flutterwave()
+ * @method static paystack()
+ * @method static payfast()
+ * @method static cashfree()
+ * @method static instamojo()
+ * @method static marcadopago()
+ * @method static payumoney()
+ * @method static squareup()
+ * @method static cinetpay()
+ * @method static paytabs()
+ * @method static zitopay()
+ * @method static toyyibpay()
+ * @method static pagalipay()
+ * @method static authorizenet()
+ *
+ *  @see InstamojoPay
+ * @method public setClientId($client_id)
+ */
+class VodiaPaymentGateway extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return 'VodiaPaymentGateway';
+    }
+}
