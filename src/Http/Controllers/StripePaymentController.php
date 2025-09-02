5c2ee15b17084f19e8b6c18e76faad0a3e034bf8
Freddy Okoye <44079004+freddyokoye@users.noreply.github.com>
2025-09-02 13:47:59 +0100

    Create StripePaymentController.php

---
+++ src/Http/Controllers/StripePaymentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Vodia\Paymentgateway\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Vodia\Paymentgateway\Facades\VodiaPaymentGateway;
+
+class StripePaymentController extends Controller
+{
+    public function charge_customer(Request $request){
+        try{
+            $stripe_session = VodiaPaymentGateway::stripe()->charge_customer_from_controller([
+                'amount' => $request->amount,
+                'charge_amount' => $request->charge_amount,
+                'title' => $request->title,
+                'description' => $request->description,
+                'ipn_url' => $request->ipn_url,
+                'order_id' => $request->order_id,
+                'track' => $request->track,
+                'cancel_url' => $request->cancel_url,
+                'success_url' => $request->success_url,
+                'email' => $request->email,
+                'name' => $request->name,
+                'payment_type' => $request->payment_type,
+                'secret_key' => $request->secret_key,
+                'currency' => $request->currency,
+            ]);
+            return response()->json(['id' => $stripe_session['id']]);
+        }catch(\Exception $e){
+            return response()->json(['msg' => $e->getMessage(),'type' => 'danger']);
+        }
+    }
+}
