854b1e42361c88f791a89857a2efbef7e9845ca4
Freddy Okoye <44079004+freddyokoye@users.noreply.github.com>
2025-09-02 13:53:29 +0100

    Create StripeTest.php

---
+++ tests/Features/StripeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Vodia\Paymentgateway\Tests\Features;
+
+use Vodia\Paymentgateway\Facades\VodiaPaymentGateway;
+use Vodia\Paymentgateway\Tests\TestCase;
+
+class StripeTest extends TestCase
+{
+//    public function testCustomerCharge(){
+//
+//    }
+//    public function testIpn(){
+//
+//    }
+    public function testSetCredentials(){
+        $stripe  = VodiaPaymentGateway::stripe();
+//        $stripe->se
+    }
+}
