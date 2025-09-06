59e83bcd1084780c8744789368c9bc0e0b1b6094
Freddy Okoye <44079004+freddyokoye@users.noreply.github.com>
2025-09-06 14:38:09 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::group(['middleware' => 'web'],function (){
     /**
      *  STRIPE PAYMENT ROUTE
      * */
-    Route::post('vhodia-gateway/authorizenet',[\Vodia\Paymentgateway\Http\Controllers\AuthorizeNetPaymentController::class,'charge_customer'])->name('vodia.payment.gateway.authorizenet');
-    Route::post('vhodia-gateway/stipe',[\Vodia\Paymentgateway\Http\Controllers\StripePaymentController::class,'charge_customer'])->name('vodia.payment.gateway.stripe');
-    Route::post('vhodia-gateway/paystack',[\Vodia\Paymentgateway\Http\Controllers\PaystackPaymentController::class,'redirect_to_gateway'])->name('vodia.payment.gateway.paystack');
-    Route::get('vhodia-gateway/paystack-callback',[\Vodia\Paymentgateway\Http\Controllers\PaystackPaymentController::class,'callback'])->name('vodia.payment.gateway.paystack.callback');
+    Route::post('vhodia-gateway/authorizenet',[\Freddyokoye\Paymentgateway\Http\Controllers\AuthorizeNetPaymentController::class,'charge_customer'])->name('vodia.payment.gateway.authorizenet');
+    Route::post('vhodia-gateway/stipe',[\Freddyokoye\Paymentgateway\Http\Controllers\StripePaymentController::class,'charge_customer'])->name('vodia.payment.gateway.stripe');
+    Route::post('vhodia-gateway/paystack',[\Freddyokoye\Paymentgateway\Http\Controllers\PaystackPaymentController::class,'redirect_to_gateway'])->name('vodia.payment.gateway.paystack');
+    Route::get('vhodia-gateway/paystack-callback',[\Freddyokoye\Paymentgateway\Http\Controllers\PaystackPaymentController::class,'callback'])->name('vodia.payment.gateway.paystack.callback');
 });
 
